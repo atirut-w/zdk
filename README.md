cb3e2ccc80f69773f65b295f601c3fcbc79adb92
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-06-22 22:31:38 +0700

    Validate input file using Clang

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ C compiler and libraries for Z80 development.
 ## Dependencies
 - [GNU binutils](https://www.gnu.org/software/binutils/) (`--target=z80-elf`) for assembling and linking. Refer to the [LURe build script](https://github.com/lure-sh/lure-repo/blob/master/binutils-z80/lure.sh) for an example of how to build binutils for the Z80 target, or use LURe to build it for you.
 - ANTLR4 for parsing C code. Your distribution of choice should already have a package for ANTLR4, but you can also download from [the official website](https://www.antlr.org/).
+- Clang for validating input code.
 
 > [!WARNING]
 > Ubuntu's package for ANTLR4 is outdated and generates invalid C++ code. You should download the JAR from the official website and use that instead.
--- cc/src/main.cpp
+++ cc/src/main.cpp
@@ -1,6 +1,9 @@
 #include <argparse/argparse.hpp>
 #include <memory>
 #include <filesystem>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace argparse;
@@ -14,6 +17,13 @@ unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
     parser->add_argument("source")
         .help("Source file")
         .action([](const string &value) { return filesystem::path(value); });
+    
+    // Include directories
+    parser->add_argument("-I", "--include")
+        .help("Add directory to include search path")
+        .action([](const string &value) { return filesystem::path(value); })
+        .nargs(1)
+        .default_value(vector<filesystem::path>{});
 
     try
     {
@@ -27,10 +37,33 @@ unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
     return parser;
 }
 
+bool validate_input(const filesystem::path &path, vector<filesystem::path> includes)
+{
+    if (!filesystem::exists(path))
+    {
+        cerr << "File not found: " << path << endl;
+        return false;
+    }
+
+    string command = "clang -fsyntax-only -nostdinc -nostdlib ";
+    for (const auto &include : includes)
+    {
+        command += "-I" + include.string() + " ";
+    }
+
+    return system((command + path.string()).c_str()) == 0;
+}
+
 int main(int argc, char *argv[])
 {
-    auto parser = parse_args(argc, argv);
-    auto path = parser->get<filesystem::path>("source");
+    auto args = parse_args(argc, argv);
+    auto path = filesystem::absolute(args->get<filesystem::path>("source"));
+    auto includes = args->get<vector<filesystem::path>>("include");
+
+    if (!validate_input(path, includes))
+    {
+        return 1;
+    }
 
     return 0;
 }
