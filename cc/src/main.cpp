b57afb1d205026c9ccf1a77265b0c3e0a091083d
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-06-22 12:28:35 +0700

    Add Argparse

--- cc/src/main.cpp
+++ cc/src/main.cpp
@@ -1,4 +1,36 @@
-int main()
+#include <argparse/argparse.hpp>
+#include <memory>
+#include <filesystem>
+
+using namespace std;
+using namespace argparse;
+
+unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
 {
+    auto parser = make_unique<ArgumentParser>("cc");
+    parser->add_description("C compiler for Z80");
+
+    // Source file
+    parser->add_argument("source")
+        .help("Source file")
+        .action([](const string &value) { return filesystem::path(value); });
+
+    try
+    {
+        parser->parse_args(argc, argv);
+    }
+    catch (const exception &e)
+    {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+    return parser;
+}
+
+int main(int argc, char *argv[])
+{
+    auto parser = parse_args(argc, argv);
+    auto path = parser->get<filesystem::path>("source");
+
     return 0;
 }
