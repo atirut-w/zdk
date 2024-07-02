06d119cebb71be3792aa9309393f9de32182bf9e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-02 22:14:23 +0700

    llc: analyzer: stub for handling function defs

--- src/llc/analyzer.cpp
+++ src/llc/analyzer.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 any Analyzer::visitCompilationUnit(LLVMIRParser::CompilationUnitContext *ctx)
 {
+    for (auto ent : ctx->topLevelEntity())
+    {
+        if (auto func_def = ent->funcDef())
+        {
+            auto info = any_cast<FunctionInfo>(visitFuncDef(func_def));
+            module_info.functions[info.name] = info;
+        }
+    }
+
     return module_info;
+}
+
+any Analyzer::visitFuncDef(LLVMIRParser::FuncDefContext *ctx)
+{
+    FunctionInfo info;
+
+    return info;
 }
--- src/llc/include/analyzer.hpp
+++ src/llc/include/analyzer.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include <LLVMIRBaseVisitor.h>
+#include <map>
+#include <string>
+
+struct FunctionInfo
+{
+    std::string name;
+    std::string type;
+};
 
 struct ModuleInfo
 {
+    std::map<std::string, FunctionInfo> functions;
 };
 
 class Analyzer : public LLVMIRBaseVisitor
@@ -10,4 +19,5 @@ class Analyzer : public LLVMIRBaseVisitor
     ModuleInfo module_info;
 
     virtual std::any visitCompilationUnit(LLVMIRParser::CompilationUnitContext *ctx) override;
+    virtual std::any visitFuncDef(LLVMIRParser::FuncDefContext *ctx) override;
 };
