1057baeda3f1120379b7670f659b678b48330ca5
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-02 21:00:49 +0700

    LLC: Parse args

--- src/llc/main.cpp
+++ src/llc/main.cpp
@@ -1,4 +1,35 @@
+#include <argparse/argparse.hpp>
+#include <filesystem>
+#include <memory>
+
+using namespace std;
+using namespace argparse;
+
+unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
+{
+    auto parser = make_unique<ArgumentParser>("llc");
+    parser->add_description("LLVM IR compiler for Z80");
+
+    // Source file
+    parser->add_argument("source").help("LLVM IR source file").action([](const string &value) {
+        return filesystem::absolute(value);
+    });
+
+    try
+    {
+        parser->parse_args(argc, argv);
+    }
+    catch (const exception &e)
+    {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+    return parser;
+}
+
 int main(int argc, char *argv[])
 {
+    auto args = parse_args(argc, argv);
+    
     return 0;
 }
