d710680d8a01ac7c0c8365b935004356ae439cf3
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-03 22:15:27 +0700

    llc: codegen: generate function stub

--- src/llc/include/codegen.hpp
+++ src/llc/include/codegen.hpp
@@ -7,6 +7,12 @@ class Codegen : public LLVMIRBaseVisitor
 {
     ModuleInfo &module_info;
     std::ostream &os;
+    FunctionInfo *current_function = nullptr;
+
+    virtual std::any visitFuncDef(LLVMIRParser::FuncDefContext *ctx) override;
+    virtual std::any visitFuncHeader(LLVMIRParser::FuncHeaderContext *ctx) override;
+
+    virtual std::any visitRetTerm(LLVMIRParser::RetTermContext *ctx) override;
 
 public:
     Codegen(ModuleInfo &module_info, std::ostream &output);
