7edbba2691985a7481b56521247f8b28f9c00a83
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-04 20:30:17 +0700

    llc: analyzer: store attribute group

--- src/llc/analyzer.cpp
+++ src/llc/analyzer.cpp
@@ -1,6 +1,13 @@
+#include "LLVMIRParser.h"
 #include <analyzer.hpp>
+#include <iostream>
 
 using namespace std;
+
+string unquote(const string &str)
+{
+    return str.substr(1, str.size() - 2);
+}
 
 any Analyzer::visitCompilationUnit(LLVMIRParser::CompilationUnitContext *ctx)
 {
@@ -12,6 +19,12 @@ any Analyzer::visitCompilationUnit(LLVMIRParser::CompilationUnitContext *ctx)
         {
             auto info = any_cast<FunctionInfo>(visitFuncDef(func_def));
             module_info.functions[info.name] = info;
+        }
+        else if (auto attr_ctx = ent->attrGroupDef())
+        {
+            int id = stoi(attr_ctx->AttrGroupId()->getText().substr(1));
+            auto group = any_cast<AttributeGroup>(visitAttrGroupDef(attr_ctx));
+            module_info.attribute_groups[id] = group;
         }
     }
 
@@ -26,7 +39,40 @@ any Analyzer::visitFuncDef(LLVMIRParser::FuncDefContext *ctx)
     info.type = header->type()->getText();
     info.name = header->GlobalIdent()->getText().substr(1);
 
+    for (auto field_ctx : header->funcHdrField())
+    {
+        if (auto attr_ctx = field_ctx->funcAttribute())
+        {
+            info.attribute_group = stoi(attr_ctx->AttrGroupId()->getText().substr(1));
+            // cout << "Assigned attribute group " << info.attribute_group << " to function " << info.name << endl;
+        }
+    }
+
     // TODO: Iterate over args and locals for stack manip
 
     return info;
+}
+
+any Analyzer::visitAttrGroupDef(LLVMIRParser::AttrGroupDefContext *ctx)
+{
+    AttributeGroup group;
+    // cout << "Visiting attribute group definition" << endl;
+
+    for (auto attr_ctx : ctx->funcAttribute())
+    {
+        if (auto pair_ctx = attr_ctx->attrPair())
+        {
+            string key = unquote(pair_ctx->StringLit()[0]->getText());
+            string value = unquote(pair_ctx->StringLit()[1]->getText());
+            group[key] = value;
+            // cout << "\t" << key << " = " << value << endl;
+        }
+        else
+        {
+            group[attr_ctx->getText()] = "";
+            // cout << "\t" << attr_ctx->getText() << endl;
+        }
+    }
+
+    return group;
 }
--- src/llc/include/analyzer.hpp
+++ src/llc/include/analyzer.hpp
@@ -2,17 +2,22 @@
 #include <LLVMIRBaseVisitor.h>
 #include <map>
 #include <string>
+#include <vector>
 
 struct FunctionInfo
 {
     std::string name;
     std::string type;
+    int attribute_group = -1;
     // TODO: Args & locals
 };
+
+typedef std::map<std::string, std::string> AttributeGroup;
 
 struct ModuleInfo
 {
     std::map<std::string, FunctionInfo> functions;
+    std::map<int, AttributeGroup> attribute_groups;
 };
 
 class Analyzer : public LLVMIRBaseVisitor
@@ -21,4 +26,5 @@ class Analyzer : public LLVMIRBaseVisitor
 
     virtual std::any visitCompilationUnit(LLVMIRParser::CompilationUnitContext *ctx) override;
     virtual std::any visitFuncDef(LLVMIRParser::FuncDefContext *ctx) override;
+    virtual std::any visitAttrGroupDef(LLVMIRParser::AttrGroupDefContext *ctx) override;
 };
