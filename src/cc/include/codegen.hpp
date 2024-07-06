c349f471be4f5a01b166772fa49d0efd3b4bf55e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-06 17:45:25 +0700

    cc: codegen: generate function stub

--- src/cc/include/codegen.hpp
+++ src/cc/include/codegen.hpp
@@ -8,6 +8,8 @@ class CodeGen : public CBaseVisitor
     ProgramMeta &program_meta;
     std::ostream &output;
 
+    virtual std::any visitFunctionDefinition(CParser::FunctionDefinitionContext *ctx) override;
+
 public:
     CodeGen(ProgramMeta &program_meta, std::ostream &output);
 };
