5de0f6609d523800e3517e66b151342534705cd1
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-07 09:49:52 +0700

    cc: nuke old stuff

--- src/cc/include/codegen.hpp
+++ src/cc/include/codegen.hpp
@@ -19,10 +19,6 @@ class CodeGen : public CBaseVisitor
     ProgramMeta &program_meta;
     std::ostream &output;
 
-    virtual std::any visitFunctionDefinition(CParser::FunctionDefinitionContext *ctx) override;
-
-    // All of these are for math expressions. Yes, all of them.
-    virtual std::any visitPrimaryExpression(CParser::PrimaryExpressionContext *ctx) override; // Your pain starts here
 public:
     CodeGen(ProgramMeta &program_meta, std::ostream &output);
 };
