15271775273d7410794ba0d7ef33fb6e387f0e69
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-15 11:01:39 +0700

    cc: analyzer: visit function definitions

--- src/cc/analyzer.cpp
+++ src/cc/analyzer.cpp
@@ -5,7 +5,19 @@ using namespace antlr4;
 
 any Analyzer::visitCompilationUnit(CParser::CompilationUnitContext *ctx)
 {
-    // TODO: Implement
-
+    visitChildren(ctx);
     return meta;
+}
+
+any Analyzer::visitFunctionDefinition(CParser::FunctionDefinitionContext *ctx)
+{
+    string name = ctx->declarator()->directDeclarator()->directDeclarator()->Identifier()->getText();
+
+    if (meta.functions.find(name) == meta.functions.end())
+    {
+        meta.functions[name] = FunctionMeta();
+    }
+    current_function = &meta.functions[name];
+
+    return visitChildren(ctx);
 }
--- src/cc/include/analyzer.hpp
+++ src/cc/include/analyzer.hpp
@@ -1,14 +1,23 @@
 #pragma once
 #include <CBaseVisitor.h>
+#include <map>
+#include <string>
+
+struct FunctionMeta
+{
+};
 
 struct ProgramMeta
 {
+    std::map<std::string, FunctionMeta> functions;
 };
 
 class Analyzer : public CBaseVisitor
 {
     ProgramMeta meta;
+    FunctionMeta *current_function = nullptr;
 
 public:
     virtual std::any visitCompilationUnit(CParser::CompilationUnitContext *ctx) override;
+    virtual std::any visitFunctionDefinition(CParser::FunctionDefinitionContext *ctx) override;
 };
