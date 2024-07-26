8a23599d2081031f5457701f87439fd678de57ab
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-26 23:16:29 +0700

    cc: add some more typing stuff

--- src/cc/include/types.hpp
+++ src/cc/include/types.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include <map>
+#include <string>
+
+struct Type
+{
+    int size;
+    ~Type() {} // Force polymorphism
+};
+
+// TODO: Flesh this out
+struct PrimitiveType : public Type
+{
+
+};
+
+// TODO: Structs
+
+extern std::map<std::string, PrimitiveType> primitives;
--- src/cc/types.cpp
+++ src/cc/types.cpp
@@ -1,1 +1,5 @@
 #include <types.hpp>
+
+std::map<std::string, PrimitiveType> primitives = {
+    // TODO: Fill this out
+};
