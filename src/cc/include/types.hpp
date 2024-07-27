a5adfc3181adfb929fbbff33750fe2bab2819c69
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-27 11:21:25 +0700

    cc: typing: add default constructor to type data classes

--- src/cc/include/types.hpp
+++ src/cc/include/types.hpp
@@ -6,6 +6,8 @@
 struct Type
 {
     int size;
+
+    Type() = default;
     Type(int size) : size{size} {}
     virtual ~Type() = default; // Force polymorphism
 };
@@ -17,6 +19,7 @@ struct PrimitiveType : public Type
     std::vector<std::string> word_layout;
     // TODO: Optional subroutine for name for sign extension
 
+    PrimitiveType() = default;
     PrimitiveType(int size, std::vector<std::string> byte_layout, std::vector<std::string> word_layout)
         : Type{size}, byte_layout{byte_layout}, word_layout{word_layout}
     {
