1e7cf9b8c578364c2809061b722130cf8f95120e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-27 10:51:07 +0700

    Remove test files
    My brain farted during the last commit in the typing branch

--- init.s
+++
@@ -1,9 +0,0 @@
-    .org 0
-    .type init, @function
-init:
-    ld hl, 0
-    ld sp, hl ; Anything put on the stack should appear all the way at 0xffff
-
-    .extern main
-    call main
-    halt ; Put breakpoint here to see if the function returns correctly
--- test.c
+++
@@ -1,5 +0,0 @@
-int main()
-{
-    char ch = 97;
-    return 0;
-}
