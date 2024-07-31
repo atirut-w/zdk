7cc2c86a61205afe5889fc7f7484b02a74d5a189
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-31 22:16:28 +0700

    cc: gut out old codes

--- src/cc/main.cpp
+++ src/cc/main.cpp
@@ -1,9 +1,4 @@
-#include <CLexer.h>
-#include <CParser.h>
-#include <analyzer.hpp>
 #include <argparse/argparse.hpp>
-#include <codegen.hpp>
-#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -12,7 +7,6 @@
 
 using namespace std;
 using namespace argparse;
-using namespace antlr4;
 
 unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
 {
@@ -36,12 +30,6 @@ unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
         .help("Keep intermediate files")
         .default_value(false)
         .implicit_value(true);
-    
-    // Dump AST
-    parser->add_argument("--dump-ast")
-        .help("Dump AST to stdout")
-        .default_value(false)
-        .implicit_value(true);
 
     try
     {
@@ -62,52 +50,17 @@ int main(int argc, char *argv[])
     filesystem::path intermediate = source;
     auto keep_intermediate = args->get<bool>("--keep-intermediate");
 
-    string clang_preamble = "exec -a zdk-cc clang -nostdinc -nostdlib ";
-    for (auto &include : args->get<vector<filesystem::path>>("--include"))
+    string cpp_preamble = "exec -a zdk-cpp clang -E -P -nostdinc -nostdinc++ ";
+    for (const auto &include : args->get<vector<filesystem::path>>("--include"))
     {
-        clang_preamble += "-I" + include.string() + " ";
+        cpp_preamble += " -I" + include.string();
     }
 
-    if (system((clang_preamble + "-fsyntax-only " + source.string()).c_str()))
+    if (system((cpp_preamble + source.string() + " > " + intermediate.replace_extension(".i").string()).c_str()) != 0)
     {
+        cerr << "Failed to preprocess source file" << endl;
         return 1;
     }
-    if (system(
-            (clang_preamble + "-E " + source.string() + " > " + intermediate.replace_extension(".i").string()).c_str()))
-    {
-        cerr << "BUG: clang -E failed despite syntax check passing" << endl;
-        return 1;
-    }
-
-    ifstream input(intermediate.replace_extension(".i"));
-    ANTLRInputStream input_stream(input);
-    CLexer lexer(&input_stream);
-    CommonTokenStream tokens(&lexer);
-    CParser parser(&tokens);
-
-    tree::ParseTree *tree = parser.compilationUnit();
-    if (lexer.getNumberOfSyntaxErrors() > 0 || parser.getNumberOfSyntaxErrors() > 0)
-    {
-        cerr << "BUG: ANTLR syntax error. mayhapes C version newer than C11?" << endl;
-        return 1;
-    }
-
-    if (!keep_intermediate)
-    {
-        filesystem::remove(intermediate.replace_extension(".i"));
-    }
-    std::ofstream output(intermediate.replace_extension(".s"));
-
-    if (args->get<bool>("--dump-ast"))
-    {
-        cout << tree->toStringTree(&parser) << endl;
-        return 0;
-    }
-
-    Analyzer analyzer;
-    ProgramMeta meta = any_cast<ProgramMeta>(analyzer.visit(tree));
-    CodeGen codegen(meta, output);
-    codegen.visit(tree);
 
     return 0;
 }
