6ed80f7f571694e7c4978a2b85e139df0c0383df
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-31 22:16:28 +0700

    cc: gut out old codes

--- src/cc/main.cpp
+++ src/cc/main.cpp
@@ -1,19 +1,12 @@
-#include <CLexer.h>
-#include <CParser.h>
-#include <analyzer.hpp>
 #include <argparse/argparse.hpp>
-#include <codegen.hpp>
-#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <config.hpp>
 
 using namespace std;
 using namespace argparse;
-using namespace antlr4;
 
 unique_ptr<const ArgumentParser> parse_args(int argc, char *argv[])
 {
@@ -73,54 +66,21 @@ int main(int argc, char *argv[])
     const auto source = args->get<filesystem::path>("source");
     filesystem::path intermediate = source;
 
-    string clang_preamble = "exec -a zdk-cc clang -nostdinc -nostdlib ";
-    for (auto &include : args->get<vector<filesystem::path>>("--include"))
+    string cpp_preamble = "exec -a zdk-cpp clang -E -P -nostdinc -nostdinc++ ";
+    for (const auto &include : args->get<vector<filesystem::path>>("--include"))
     {
-        clang_preamble += "-I" + include.string() + " ";
+        cpp_preamble += " -I" + include.string();
     }
 
-#ifdef CLANG_VALIDATION_ENABLED
-    if (system((clang_preamble + "-fsyntax-only " + source.string()).c_str()))
+    if (system((cpp_preamble + source.string() + " > " + intermediate.replace_extension(".i").string()).c_str()) != 0)
     {
-        return 1;
-    }
-#endif
-    if (system(
-            (clang_preamble + "-E " + source.string() + " > " + intermediate.replace_extension(".i").string()).c_str()))
-    {
-        cerr << "BUG: clang -E failed despite syntax check passing" << endl;
+        cerr << "Failed to preprocess source file" << endl;
         return 1;
     }
     if (args->get<bool>("-E"))
     {
         return 0;
     }
-
-    ifstream input(intermediate.replace_extension(".i"));
-    ANTLRInputStream input_stream(input);
-    CLexer lexer(&input_stream);
-    CommonTokenStream tokens(&lexer);
-    CParser parser(&tokens);
-
-    filesystem::remove(intermediate.replace_extension(".i"));
-    tree::ParseTree *tree = parser.compilationUnit();
-    if (lexer.getNumberOfSyntaxErrors() > 0 || parser.getNumberOfSyntaxErrors() > 0)
-    {
-        cerr << "BUG: ANTLR syntax error. mayhapes C version newer than C11?" << endl;
-        return 1;
-    }
-    if (args->get<bool>("--dump-ast"))
-    {
-        cout << tree->toStringTree(&parser, true) << endl;
-        return 0;
-    }
-
-    std::ofstream output(intermediate.replace_extension(".s"));
-
-    Analyzer analyzer;
-    Module module = any_cast<Module>(analyzer.visit(tree));
-    CodeGen codegen(module, output);
-    codegen.visit(tree);
 
     if (args->get<bool>("-S"))
     {
--- src/cc/types.cpp
+++
@@ -1,3 +0,0 @@
-#include <types.hpp>
-
-using namespace std;
