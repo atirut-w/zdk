b42ea0ce3569e8ee2a14416d38b3b771b08172e7
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-07-31 22:37:09 +0700

    cc: implement lexer input loop

--- src/cc/lexer.cpp
+++ src/cc/lexer.cpp
@@ -5,6 +5,11 @@ using namespace std;
 vector<unique_ptr<Token>> Lexer::tokenize()
 {
     vector<unique_ptr<Token>> tokens;
-    
+
+    while (input.peek() != EOF)
+    {
+        char c = input.get();
+    }
+
     return tokens;
 }
