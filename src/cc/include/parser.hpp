ee839099f3fae5c30eda128e9e06a85559e4036b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-08-03 09:35:00 +0700

    cc: parser: AST node stubs

--- src/cc/include/parser.hpp
+++ src/cc/include/parser.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#include <lexer.hpp>
+#include <memory>
+#include <vector>
+
+struct ASTNode
+{
+    std::vector<std::shared_ptr<ASTNode>> children;
+    virtual ~ASTNode() = default; // Force polymorphism
+};
+
+struct Expression : ASTNode
+{
+    Token *integer = nullptr;
+};
+
+struct Statement : ASTNode
+{
+    Expression *exp = nullptr;
+};
+
+struct FunctionDefinition : ASTNode
+{
+    Token *identifier = nullptr;
+    Statement *body = nullptr;
+};
+
+struct Program : ASTNode
+{
+    FunctionDefinition *main = nullptr;
+};
