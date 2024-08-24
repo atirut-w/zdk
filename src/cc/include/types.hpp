1bb75d0e55b2d39648cc784269eee7b8fc6e706b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-08-24 23:31:03 +0700

    cc: remove old TODO

--- src/cc/include/types.hpp
+++ src/cc/include/types.hpp
@@ -7,10 +7,9 @@ struct Type
     virtual ~Type() = default; // Force polymorphism
 };
 
-// TODO: Make a drop-in replacement for this typedef. Custom class, maybe?
+// TODO: Make a drop-in replacement for this typedef to allow storing unowned pointers. Custom class, maybe?
 typedef std::shared_ptr<Type> ParsedType;
 
-// TODO: Flesh this out
 struct PrimitiveType : public Type
 {
     enum Kind
