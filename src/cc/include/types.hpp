96b96f3e8bde6c3e97723839b1d550ef1a303c32
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-08-24 22:55:20 +0700

    cc: introduce type for functions

--- src/cc/include/types.hpp
+++ src/cc/include/types.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <memory>
+#include <vector>
 
 struct Type
 {
     virtual ~Type() = default; // Force polymorphism
 };
 
+// TODO: Make a drop-in replacement for this typedef. Custom class, maybe?
 typedef std::shared_ptr<Type> ParsedType;
 
 // TODO: Flesh this out
@@ -19,6 +21,12 @@ struct PrimitiveType : public Type
         Int,
     } kind;
     bool is_signed = false;
+};
+
+struct FunctionType : public Type
+{
+    ParsedType return_type;
+    std::vector<ParsedType> parameters;
 };
 
 // TODO: Structs
