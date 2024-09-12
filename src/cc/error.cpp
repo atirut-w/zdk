9b7d156ce0f72f9b74db442e22425b8d4916c7de
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-13 00:24:24 +0700

    cc: point out error location

--- src/cc/error.cpp
+++ src/cc/error.cpp
@@ -1,5 +1,7 @@
 #include "Parser.h"
+#include <cstdio>
 #include <error.hpp>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -8,8 +10,15 @@ using namespace antlr4;
 void CCErrorListener::syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line,
                                   size_t charPositionInLine, const std::string &msg, std::exception_ptr e)
 {
-    string source = recognizer->getInputStream()->getSourceName();
+    ifstream file(recognizer->getInputStream()->getSourceName());
 
-    cout << "\033[1m" << source << ":" << line << ":" << charPositionInLine << ": \033[31merror: \033[0;1m" << msg
-         << "\033[0m" << endl;
+    string source = recognizer->getInputStream()->getSourceName();
+    string lineText;
+    for (size_t i = 0; i < line; i++)
+        getline(file, lineText);
+
+    printf("\033[1m%s:%zu:%zu: \033[1;31merror: \033[0;1m%s\033[0m\n", source.c_str(), line, charPositionInLine,
+           msg.c_str());
+    printf("%5zu | %s\n", line, lineText.c_str());
+    printf("%*s | %*s\033[1;31m^\033[0m\n", 5, "", static_cast<int>(charPositionInLine), "");
 }
