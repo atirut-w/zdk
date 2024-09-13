f2516ac16109fa74a2b682d1dac8969dc71ecaaf
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-13 11:59:58 +0700

    cc: move callout macros to header file

--- src/cc/error.cpp
+++ src/cc/error.cpp
@@ -4,13 +4,6 @@
 #include <error.hpp>
 #include <fstream>
 
-// Source name, line, column, color, type, message
-#define CALLOUT_FMT "\033[1m%s:%zu:%zu: \033[1;%dm%s: \033[0;1m%s\033[0m\n"
-// Line number, line text
-#define CALLOUT_INFO_FMT "%5zu | %s\n"
-// Gutter width, gutter text, arrow position, arrow text, color
-#define CALLOUT_ARROW_FMT "%*s | %*s\033[1;%dm^\033[0m\n"
-
 using namespace std;
 using namespace antlr4;
 
--- src/cc/include/error.hpp
+++ src/cc/include/error.hpp
@@ -1,6 +1,13 @@
 #pragma once
 #include "BaseErrorListener.h"
 
+// Source name, line, column, color, type, message
+#define CALLOUT_FMT "\033[1m%s:%zu:%zu: \033[1;%dm%s: \033[0;1m%s\033[0m\n"
+// Line number, line text
+#define CALLOUT_INFO_FMT "%5zu | %s\n"
+// Gutter width, gutter text, arrow position, arrow text, color
+#define CALLOUT_ARROW_FMT "%*s | %*s\033[1;%dm^\033[0m\n"
+
 class CCErrorListener : public antlr4::BaseErrorListener
 {
     virtual void syntaxError(antlr4::Recognizer *recognizer, antlr4::Token *offendingSymbol, size_t line,
