89b826396920216105745106a1f31a2a99600d76
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-21 16:43:40 +0700

    cc: codegen: implement return

--- src/cc/include/codegen.hpp
+++ src/cc/include/codegen.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "zir/instruction.hpp"
 #include "zir/module.hpp"
 #include <ostream>
 
@@ -10,5 +11,9 @@ class Codegen
 public:
     Codegen(std::ostream &out, const ZIR::Module &module);
 
+    void load(const ZIR::Operand &operand);
+
     void generate();
+    void generate_function(const ZIR::Module::Function &function);
+    void generate_instruction(const ZIR::Instruction &instruction);
 };
