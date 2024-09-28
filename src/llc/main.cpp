e572c4eb6e9aa3cca70272553d05b18cbec7f595
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-29 00:41:11 +0700

    llc: format document

--- src/llc/main.cpp
+++ src/llc/main.cpp
@@ -5,34 +5,28 @@
 using namespace std;
 using namespace argparse;
 
-unique_ptr<const ArgumentParser> parse_args(int argc, char **argv)
-{
-    auto parser = make_unique<ArgumentParser>("llc");
+unique_ptr<const ArgumentParser> parse_args(int argc, char **argv) {
+  auto parser = make_unique<ArgumentParser>("llc");
 
-    parser->add_argument("input").help("Input file").action([](const string &value) {
-        return filesystem::path(value);
-    });
+  parser->add_argument("input")
+      .help("Input file")
+      .action([](const string &value) { return filesystem::path(value); });
 
-    try
-    {
-        parser->parse_args(argc, argv);
-        return parser;
-    }
-    catch (const runtime_error &e)
-    {
-        cerr << e.what() << endl;
-        cerr << *parser << endl;
-    }
-    return nullptr;
+  try {
+    parser->parse_args(argc, argv);
+    return parser;
+  } catch (const runtime_error &e) {
+    cerr << e.what() << endl;
+    cerr << *parser << endl;
+  }
+  return nullptr;
 }
 
-int main(int argc, char **argv)
-{
-    auto args = parse_args(argc, argv);
-    if (!args)
-    {
-        return 1;
-    }
+int main(int argc, char **argv) {
+  auto args = parse_args(argc, argv);
+  if (!args) {
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
