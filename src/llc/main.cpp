718a96d18b2ea5e90b3774599a32f23a2cd21aa5
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-09-29 00:34:02 +0700

    llc: add argument parsing

--- src/llc/main.cpp
+++ src/llc/main.cpp
@@ -1,3 +1,38 @@
-int main() {
-  return 0;
+#include "argparse/argparse.hpp"
+#include <filesystem>
+#include <memory>
+
+using namespace std;
+using namespace argparse;
+
+unique_ptr<const ArgumentParser> parse_args(int argc, char **argv)
+{
+    auto parser = make_unique<ArgumentParser>("llc");
+
+    parser->add_argument("input").help("Input file").action([](const string &value) {
+        return filesystem::path(value);
+    });
+
+    try
+    {
+        parser->parse_args(argc, argv);
+        return parser;
+    }
+    catch (const runtime_error &e)
+    {
+        cerr << e.what() << endl;
+        cerr << *parser << endl;
+    }
+    return nullptr;
+}
+
+int main(int argc, char **argv)
+{
+    auto args = parse_args(argc, argv);
+    if (!args)
+    {
+        return 1;
+    }
+
+    return 0;
 }
