2772b4656698012fe436b233660045f4776ffa65
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-06 17:31:51 +0700

    sdcc wrapper: improve argument parsing

--- src/sdcc-wrapper/main.cpp
+++ src/sdcc-wrapper/main.cpp
@@ -1,7 +1,6 @@
 #include "argparse/argparse.hpp"
 #include <cstdlib>
 #include <filesystem>
-#include <iostream>
 #include <memory>
 #include <sched.h>
 #include <stdexcept>
@@ -14,13 +13,16 @@
 using namespace std;
 using namespace argparse;
 
-pair<unique_ptr<const ArgumentParser>, vector<string>> parse_args(int argc, char *argv[]) {
-  auto parser = make_unique<ArgumentParser>("zdk-sdcc", "1.0", default_arguments::none);
+pair<unique_ptr<const ArgumentParser>, vector<string>>
+parse_args(int argc, char *argv[]) {
+  auto parser =
+      make_unique<ArgumentParser>("zdk-sdcc", "1.0", default_arguments::none);
 
   parser->add_argument("filename");
-  parser->add_argument("-S");
-  parser->add_argument("-c", "--compile-only");
-  parser->add_argument("-E", "--preprocessonly"); // Someone at SDCC got drunk writing that one
+  parser->add_argument("-S").flag();
+  parser->add_argument("-c", "--compile-only").flag();
+  parser->add_argument("-E", "--preprocessonly")
+      .flag(); // Someone at SDCC got drunk writing that one
 
   auto unknown = parser->parse_known_args(argc, argv);
   return {std::move(parser), std::move(unknown)};
@@ -33,13 +35,13 @@ int run(string program, vector<string> args) {
     throw runtime_error("failed to fork");
   } else if (pid == 0) {
     vector<char *> c_args;
-    c_args.push_back(const_cast<char *>(program.data()));
+    c_args.push_back(const_cast<char *>(program.c_str()));
     for (auto &arg : args) {
-      c_args.push_back(const_cast<char *>(arg.data()));
+      c_args.push_back(const_cast<char *>(arg.c_str()));
     }
     c_args.push_back(nullptr);
 
-    execvp(program.data(), c_args.data());
+    execvp(program.c_str(), c_args.data());
     _exit(EXIT_FAILURE);
   } else {
     int status;
@@ -52,17 +54,36 @@ int main(int argc, char *argv[]) {
   auto [args, unknown] = parse_args(argc, argv);
   filesystem::path filename = args->get<string>("filename");
 
-  vector<string> sdcc_args;
-  sdcc_args.push_back("-S");
-  sdcc_args.push_back(filename);
+  vector<string> sdcc_args = {"-mz80", "-S", filename};
+  if (args->get<bool>("-E")) {
+    sdcc_args.push_back("-E");
+  }
+
   if (argc > 1) {
     sdcc_args.insert(sdcc_args.end(), unknown.begin(), unknown.end());
   }
-  run("sdcc-sdcc", sdcc_args);
+  int status = run("sdcc-sdcc", sdcc_args);
+  if (status != 0) {
+    return status;
+  }
 
-  if (args->get<bool>("-S")) {
+  if (args->get<bool>("-S") || args->get<bool>("-E")) {
     return 0;
   }
+
+  system(("z80-elf-as -sdcc " + filename.replace_extension(".asm").string() +
+          " -o " + filename.replace_extension(".o").string())
+             .c_str());
+  filesystem::remove(filename.replace_extension(".asm"));
+
+  if (args->get<bool>("-c")) {
+    return 0;
+  }
+
+  system(
+      ("z80-elf-ld " + filename.replace_extension(".o").string() + " -o a.out")
+          .c_str());
+  filesystem::remove(filename.replace_extension(".o"));
 
   return 0;
 }
