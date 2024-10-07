2bc0b1ed954e0793895a193fc146308ab612895b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-07 18:49:36 +0700

    LibC: stub `puts`

---
+++ src/libc/stubs.s
@@ -0,0 +1,11 @@
+    ; Stub LibC functions, to be overridden by platform implementations
+
+.macro STUB name
+    .weak \name
+    .type \name, %function
+\name:
+    ld hl, -1
+    ret
+.endm
+
+    STUB _puts
