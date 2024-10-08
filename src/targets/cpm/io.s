f1be23ae204b745243e57ac90229063806854041
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-08 16:12:17 +0700

    CP/M: Implement `putchar()`

--- src/targets/cpm/io.s
+++ src/targets/cpm/io.s
@@ -1,4 +1,16 @@
     .equ BDOS, 5
+
+    .global _putchar
+    .type _putchar, @function
+_putchar:
+    push ix
+    ld ix, 0
+    add ix, sp
+    ld c, 2
+    ld e, (ix+4)
+    call BDOS
+    pop ix
+    ret
     
     .global _puts
     .type _puts, @function
