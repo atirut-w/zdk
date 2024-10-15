cd01fd51cbaedd2a03cde8fa811cad7d9516bfc4
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-15 10:04:36 +0700

    cc: implement argument parsing

--- src/cc/main.cpp
+++ src/cc/main.cpp
@@ -1,3 +1,39 @@
+#include "argparse/argparse.hpp"
+#include <filesystem>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+using namespace argparse;
+
+using ParsedArgs =
+    tuple<unique_ptr<const ArgumentParser>, const vector<string>>;
+
+optional<ParsedArgs> parse_args(int argc, char *argv[]) {
+  auto parser = make_unique<ArgumentParser>();
+
+  parser->add_argument("file")
+      .help("C source file to compile")
+      .action([](const string &value) { return filesystem::path(value); });
+
+  try {
+    auto remaining = parser->parse_known_args(argc, argv);
+    return {{std::move(parser), std::move(remaining)}};
+  } catch (const runtime_error &e) {
+    cerr << e.what() << endl;
+    cerr << *parser << endl;
+    return nullopt;
+  }
+}
+
 int main(int argc, char *argv[]) {
-  return 0;
+  auto expected_args = parse_args(argc, argv);
+  if (!expected_args) {
+    return 1;
+  }
+  auto &[parser, remaining] = *expected_args;
 }
