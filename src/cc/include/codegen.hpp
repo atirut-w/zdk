84f9d1118b9f054ebaaf26e851c0d15e957f2758
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-10-17 19:55:22 +0700

    cc: implement generating functions and returns

--- src/cc/include/codegen.hpp
+++ src/cc/include/codegen.hpp
@@ -4,6 +4,11 @@
 class Codegen : public CBaseVisitor {
   std::ostream &os;
 
+  virtual std::any visitTranslationUnit(CParser::TranslationUnitContext *ctx) override;
+  virtual std::any visitFunctionDefinition(CParser::FunctionDefinitionContext *ctx) override;
+  virtual std::any visitReturnStatement(CParser::ReturnStatementContext *ctx) override;
+  virtual std::any visitExpression(CParser::ExpressionContext *ctx) override;
+
 public:
   Codegen(std::ostream &os) : os(os) {}
 };
