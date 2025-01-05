0942f1c6b9c9b62d7fa2bf4abe4ab5837b514d40
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-01-05 20:35:13 +0700

    Libc: save stack frame in `__libc_start_main`

--- src/libc/src/start.s
+++ src/libc/src/start.s
@@ -3,25 +3,26 @@
 
     ; void __libc_start_main(int (*main)(int, char**, char**), int argc, char **ubp_av, void (*init)(void), void (*fini)(void), void (*rtld_fini)(void), void *stack_end)
     ; Argument locations:
-    ;   IX+2: main
-    ;   IX+4: argc
-    ;   IX+6: ubp_av
-    ;   IX+8: init
-    ;   IX+10: fini
-    ;   IX+12: rtld_fini
-    ;   IX+14: stack_end
+    ;   IX+4: main
+    ;   IX+6: argc
+    ;   IX+8: ubp_av
+    ;   IX+10: init
+    ;   IX+12: fini
+    ;   IX+14: rtld_fini
+    ;   IX+16: stack_end
     ; Variable locations:
     ;   IX-2: return value from main
     .global __libc_start_main
 __libc_start_main:
+    push ix
     ld ix, 0
     add ix, sp
 
     ; TODO: atexit(fini) & atexit(rtld_fini)
 
     ; Call init
-    ld l, (ix+8)
-    ld h, (ix+9)
+    ld l, (ix+10)
+    ld h, (ix+11)
     push hl
     ld a, l
     or h
@@ -30,14 +31,14 @@ __libc_start_main:
     ; Call main and save return value
     ld hl, 0
     ex (sp), hl ; No support for environment variables (yet)
+    ld l, (ix+8)
+    ld h, (ix+9)
+    push hl
     ld l, (ix+6)
     ld h, (ix+7)
     push hl
     ld l, (ix+4)
     ld h, (ix+5)
-    push hl
-    ld l, (ix+2)
-    ld h, (ix+3)
     push hl
     call __call_ptr
     ld (ix-2), l
@@ -46,16 +47,16 @@ __libc_start_main:
     pop bc
 
     ; TODO: Again, atexit(fini)
-    ld l, (ix+10)
-    ld h, (ix+11)
+    ld l, (ix+12)
+    ld h, (ix+13)
     ld a, l
     or h
     ex (sp), hl
     call nz, __call_ptr
 
     ; TODO: atexit(rtld_fini)
-    ld l, (ix+12)
-    ld h, (ix+13)
+    ld l, (ix+14)
+    ld h, (ix+15)
     ld a, l
     or h
     ex (sp), hl
