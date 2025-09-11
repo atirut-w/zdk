d823c48284061f7c85d832ca01ea78f52e537807
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-09-11 22:19:11 +0700

    Add Argparse

--- cc/main.cpp
+++ cc/main.cpp
@@ -1,1 +1,35 @@
-int main(int argc, char **argv) { return 0; }
+#include <argparse/argparse.hpp>
+#include <filesystem>
+#include <iostream>
+#include <memory>
+
+using argparse::ArgumentParser;
+
+std::unique_ptr<ArgumentParser> parseArguments(int argc, char **argv) {
+  auto parser = std::make_unique<ArgumentParser>("zdk-cc");
+
+  parser->add_argument("input")
+      .help("Input file to compile")
+      .action(
+          [](const std::string &arg) { return std::filesystem::path(arg); });
+
+  parser->add_argument("-S").help("Compile to assembly only").flag();
+
+  try {
+    parser->parse_args(argc, argv);
+    return parser;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << *parser;
+    return nullptr;
+  }
+}
+
+int main(int argc, char **argv) {
+  auto parser = parseArguments(argc, argv);
+  if (!parser) {
+    return 1;
+  }
+
+  return 0;
+}
