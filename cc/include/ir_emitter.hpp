b84209f9e41a143aaa39e16d23660c63bd128291
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-09-19 19:39:49 +0700

    Implement LLVM IR codegen

--- cc/include/ir_emitter.hpp
+++ cc/include/ir_emitter.hpp
@@ -2,12 +2,44 @@
 #include <any>
 #include <cparse/visitor.hpp>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
+#include <vector>
 
-struct IREmitter : public cparse::Visitor<std::any> {
+struct Symbol {
+  std::string name;
+  llvm::Value *value;
+  llvm::Type *type;
+};
+
+class IREmitter : public cparse::Visitor<std::any> {
+  llvm::LLVMContext &context;
   llvm::Module &module;
   llvm::IRBuilder<> builder;
 
+  std::vector<Symbol> symbols;
+
+  Symbol *getSymbol(const std::string &name);
+
+public:
   IREmitter(llvm::LLVMContext &context, llvm::Module &module)
-      : module(module), builder(context) {}
+      : context(context), module(module), builder(context) {}
+
+  using cparse::Visitor<std::any>::visit;
+
+  std::any visit(cparse::FunctionDefinition &node) override;
+  std::any visit(cparse::Block &node) override;
+  std::any visit(cparse::Declaration &node) override;
+  
+  std::any visit(cparse::ReturnStatement &node) override;
+  std::any visit(cparse::ExpressionStatement &node) override;
+  std::any visit(cparse::IfStatement &node) override;
+  std::any visit(cparse::CompoundStatement &node) override;
+
+  std::any visit(cparse::ConstantExpression &node) override;
+  std::any visit(cparse::IdentifierExpression &node) override;
+  std::any visit(cparse::UnaryExpression &node) override;
+  std::any visit(cparse::BinaryExpression &node) override;
+  std::any visit(cparse::AssignmentExpression &node) override;
+  std::any visit(cparse::ConditionalExpression &node) override;
 };
