b0c6839662d52212cc9e9af4f7baec447fe033c9
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-09-21 16:05:29 +0700

    Gut codegen

--- cc/include/codegen.hpp
+++ cc/include/codegen.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cparse/ast.hpp>
+#include <cparse/visitor.hpp>
 #include <ostream>
 
 struct Symbol {
@@ -17,61 +18,17 @@ struct ScopeMark {
   int bytes_in_block; // total bytes allocated for this block
 };
 
-class CodeGen {
+class CodeGen : public cparse::Visitor<void> {
   std::ostream &out;
-  int next_label = 0;
-  // NOTE: Includes ALL symbols, local and global
-  std::vector<std::unique_ptr<Symbol>> symbols;
-  cparse::FunctionDefinition *current_function = nullptr;
-
-  // Single-pass local allocation state
-  int frame_local_off = 0; // next negative offset from IX (0, -2, -4, ...)
-  std::vector<ScopeMark> scope_stack;
-
-  int generate_label() { return next_label++; }
-  Symbol *find_symbol(const std::string &name) {
-    for (auto it = symbols.end(); it != symbols.begin();) {
-      --it;
-      if ((*it)->name == name) {
-        return it->get();
-      }
-    }
-    return nullptr;
-  }
-  std::string format_ix(int offset) {
-    if (offset == 0) {
-      return "(ix)";
-    } else if (offset > 0) {
-      return std::format("(ix+{})", offset);
-    } else {
-      return std::format("(ix-{})", -offset);
-    }
-  }
-
-  // Single-pass local allocation helpers
-  void adjust_sp(int bytes);
-  void enter_scope();
-  void leave_scope();
-  int allocate_block_locals(
-      const std::vector<std::unique_ptr<cparse::Declaration>> &decls);
-  int size_of_int() { return 2; } // placeholder; expand when adding types
 
 public:
   CodeGen(std::ostream &out) : out(out) {}
 
-  void visit(cparse::TranslationUnit &tu);
-  void visit(cparse::FunctionDefinition &func);
-  void visit(cparse::Block &block);
-  void visit(cparse::Statement &stmt);
-  void visit(cparse::ReturnStatement &ret);
-  void visit(cparse::IfStatement &if_stmt);
-  void visit(cparse::CompoundStatement &compound);
+  using cparse::Visitor<void>::visit;
 
-  void visit(cparse::Expression &expr, bool rhs = false);
-  void visit(cparse::ConstantExpression &const_expr, bool rhs = false);
-  void visit(cparse::UnaryExpression &unary_expr, bool rhs = false);
-  void visit(cparse::BinaryExpression &bin_expr, bool rhs = false);
-  void visit(cparse::AssignmentExpression &assign_expr, bool rhs = false);
-  void visit(cparse::IdentifierExpression &id_expr, bool rhs = false);
-  void visit(cparse::ConditionalExpression &cond_expr, bool rhs = false);
+  void visit(cparse::TranslationUnit &node) override;
+  void visit(cparse::FunctionDefinition &node) override;
+  void visit(cparse::ReturnStatement &node) override;
+
+  void visit(cparse::ConstantExpression &node) override;
 };
