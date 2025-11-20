2105437b817b5cd8c0430081a5bfcc8f3e304136
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-11-20 21:39:15 +0700

    Put in C90 lexer and parser

--- cc/src/main.c
+++ cc/src/main.c
@@ -1,3 +1,24 @@
+#include <stdio.h>
+
+extern int yyparse(void);
+extern FILE *yyin;
+
 int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    yyin = fopen(argv[1], "r");
+    if (!yyin) {
+      fprintf(stderr, "Error: cannot open file '%s'\n", argv[1]);
+      return 1;
+    }
+  } else {
+    yyin = stdin;
+  }
+
+  yyparse();
+
+  if (yyin != stdin) {
+    fclose(yyin);
+  }
+
   return 0;
 }
