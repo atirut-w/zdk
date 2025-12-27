23731d1b41cc107d172eadb415d4c97879e1ff5b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-12-27 19:25:35 +0700

    Deal with CodeQL leftovers

--- _codeql_build_dir/cc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/zdk/zdk/cc
-# Build directory: /home/runner/work/zdk/zdk/_codeql_build_dir/cc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- _codeql_build_dir/cc1/CMakeFiles/cc1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for cc1.
-# This may be replaced when dependencies are built.
