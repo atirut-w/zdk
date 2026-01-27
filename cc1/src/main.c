f62c827ff4633dc4c4eeb93556ab0eb7ab97a759
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2026-01-27 14:45:08 +0700

    Open input file

--- cc1/src/main.c
+++ cc1/src/main.c
@@ -1,3 +1,21 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char *argv[]) {
+  FILE *input;
+
+  if (argc < 2) {
+    printf("Usage: cc1 <source-file>\n");
+    return 1;
+  }
+
+  input = fopen(argv[1], "r");
+  if (!input) {
+    fprintf(stderr, "Could not open '%s': %s\n", argv[1], strerror(errno));
+    return 1;
+  }
+
+  fclose(input);
   return 0;
 }
